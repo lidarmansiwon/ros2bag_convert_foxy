4474d03691839fb2693ba4da8bd067072bf0af40
lidarmansiwon <kimsanmaro@naver.com>
2024-01-11 15:37:49 +0900

    modified main.py for foxy
    Signed-off-by: lidarmansiwon <kimsanmaro@naver.com>

--- ros2bag_convert/main.py
+++ ros2bag_convert/main.py
@@ -1,5 +1,5 @@
-from .ros2bag_convert import read_bag
-import sys, os
+from .ros2bag_convert import read_bag,save_csv_file
+import sys
 
 argvs = sys.argv
 argc = len(argvs)
@@ -11,8 +11,12 @@ def main():
         print('Usage: #ros2bag-convert xxx.db3')
         quit()
     file_url = argvs[1]
-    read_bag.read_write_from_all_topics(file_url,True)
-
+    data = read_bag.read_from_all_topics(file_url,True)
+    for i in range(len(data)):
+        topic_name,topic_type = data[i][0],data[i][1]
+        save_csv_file.save_csv_file(data[i][2:],file_url[:file_url.rfind("/")]+".csv")
+        ##save_csv_file.save_csv_file(data[i][2:],"test4.csv")
+        
 if __name__ == '__main__':
     main()
 
